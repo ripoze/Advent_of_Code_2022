7020a4bd2160f00f00148795a8e24753f7cafb77
juha <juha.ayvari@gmail.com>
2022-12-01 13:14:28 +0200

    day01

--- day01/index.js
+++ day01/index.js
@@ -1,10 +1,10 @@
 var data = require('fs').readFileSync('day01/input.txt', 'utf8')
-data = data.split('\n\n').map(s=>s.split('\n').map(Number))
+data = data.split('\n\n').map(s => s.split('\n').map(Number))
 
-let elves = data.map(arr => arr.reduce((sum, n) => sum + n, 0)).sort((a,b)=> b-a)
+let elves = data.map(arr => arr.reduce((sum, n) => sum + n, 0)).sort((a, b) => b - a)
 
 //Part 1
 console.log(`Part 1: ${elves[0]}`) //72240
 
 //Part 2
-console.log(`Part 2: ${elves[0]+elves[1]+elves[2]}`) //210957
+console.log(`Part 2: ${elves[0] + elves[1] + elves[2]}`) //210957
